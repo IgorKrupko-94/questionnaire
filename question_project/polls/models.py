08647f041a0d1bccc68d034bccd8be3323d55d08
Igor <igor.krupcko2010@yandex.ru>
2023-03-04 02:22:28 +0300

    ДобÐавлены модели

--- question_project/polls/models.py
+++ question_project/polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(
+        'Вопрос',
+        max_length=200
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации'
+    )
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(
+        'Ответ',
+        max_length=200
+    )
+    votes = models.IntegerField(
+        'Балл',
+        default=0
+    )
