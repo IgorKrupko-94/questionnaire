ec43b869a1a30937f5a9a669976e5f195048b31d
Igor <igor.krupcko2010@yandex.ru>
2023-03-04 17:41:42 +0300

    Добавлены view-функции

--- question_project/polls/models.py
+++ question_project/polls/models.py
@@ -4,23 +4,46 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(
         'Вопрос',
-        max_length=200
+        max_length=200,
+        help_text='Задайте вопрос'
     )
     pub_date = models.DateTimeField(
-        'Дата публикации'
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
     )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return self.question_text[:40]
 
 
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='choices',
+        verbose_name='Вопрос',
+        help_text='Выберите вопрос'
     )
     choice_text = models.CharField(
         'Ответ',
-        max_length=200
+        max_length=200,
+        help_text='Введите вариант ответа'
     )
     votes = models.IntegerField(
         'Балл',
-        default=0
+        default=0,
+        help_text='Введите количество баллов'
     )
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответов'
+
+    def __str__(self):
+        return self.choice_text[:40]
