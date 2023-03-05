482dc6285c7b1637612e716f17bf77d5ef301ea8
Igor <igor.krupcko2010@yandex.ru>
2023-03-06 02:14:26 +0300

    Добавлены шаблоны и изменения в моделяхÑ

--- question_project/polls/admin.py
+++ question_project/polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    search_fields = ('question_text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+    list_editable = ('question',)
+    search_fields = ('choice_text',)
+    list_filter = ('question',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- question_project/polls/views.py
+++ question_project/polls/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.all()
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
